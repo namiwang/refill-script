f3fdb03e03c38c70bb6688dd851896f2862e6be5
Nano.WANG <namiheike@gmail.com>
2017-01-06 15:07:02 +0800

    WIP on simple translator

--- src/refill/translator.rb
+++ src/refill/translator.rb
@@ -13,21 +13,31 @@ module Refill
 
       case node
       when AST::Expressions
-        node.expressions.each do |e|
-          @output_file.puts translate e
-        end
-        return
-      when AST::Expression
-        case node
-        when AST::Addition
-          return "#{ translate node.left } + #{ translate node.right }"
-        when AST::Integer
-          return node.value.to_s
-        end
+        translate_expressions node
       end
     end
 
     private
+
+    def translate_expressions expressions
+      expressions.expressions.each do |e|
+        case e
+        when AST::Expressions
+          translate_expressions e
+        when AST::Expression
+          @output_file.write "#{translate_expression e}\n"
+        end
+      end
+    end
+
+    def translate_expression expression
+      case expression
+      when AST::Addition
+        return "( #{translate_expression expression.left} + #{translate_expression expression.right} )"
+      when AST::Integer
+        return "( #{expression.value.to_s} )"
+      end
+    end
 
   end
 end
